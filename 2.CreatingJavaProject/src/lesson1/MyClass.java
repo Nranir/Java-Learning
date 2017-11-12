4155e1a020ed525fa67bd9b95d41efe5aea374ac
Marcin <nranir@gmail.com>
2017-11-13 00:28:32 +0100

    2nd vid about java programming

---
+++ 2.CreatingJavaProject/src/lesson1/MyClass.java
@@ -0,0 +1,18 @@
+package lesson1;
+
+public class MyClass 
+{
+	/*
+	 * Test Comment 
+	 * 
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String x= "lol";
+		System.out.println("Hello World #2");
+		System.out.println(x);
+		
+	}
+
+}
